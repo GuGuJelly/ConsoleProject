28b234f30f0b48bb3502377e76daf16d76ebce6a
GuGuJelly <jellypuding@naver.com>
2024-07-18 23:55:44 +0900

    초보숫자야구
    숫자야구 어설프게 만들어보기...

---
+++ Report_NumberBaseball/Program.cs
@@ -0,0 +1,69 @@
+﻿using System.ComponentModel.Design;
+
+namespace Report_NumberBaseball
+{
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            int cNumber1 = 3;
+            int cNumber2 = 4;
+            int cNumber3 = 5;
+            Console.WriteLine($"{cNumber1}{cNumber2}{cNumber3}");
+            Console.WriteLine("컴퓨터는 3개의 숫자를 기억했답니다. 인간님 숫자를 입력해주세요 : ");
+
+            for (int i = 1; i < 11; i++)
+            {
+                Console.WriteLine($"{i}번째 이닝 시작!");
+                Console.WriteLine("인간님 3개의 숫자를 입력 부탁드립니다 : ");
+                int.TryParse(Console.ReadLine(), out int input1);
+                int.TryParse(Console.ReadLine(), out int input2);
+                int.TryParse(Console.ReadLine(), out int input3);
+                Console.WriteLine($"{input1}{input2}{input3}");
+                if (input1 == cNumber1 && input2 == cNumber2 && input3 == cNumber3)
+                {
+                    Console.WriteLine("3스트라이크!");
+                    Console.WriteLine("축하드립니다. 게임에서 이겼습니다.");
+                    break;
+                }
+                else
+                {
+                    if (input1 != cNumber1)
+                    {
+                        Console.Write("볼, ");
+                    }
+                    else if (input1 == cNumber1)
+                    {
+                        Console.Write("스트라이크, ");
+                    }
+                    if (input2 != cNumber2)
+                    {
+                        Console.Write("볼, ");
+                    }
+                    else if (input2 == cNumber2)
+                    {
+                        Console.Write("스트라이크, ");
+                    }
+                    if (input3 != cNumber3)
+                    {
+                        Console.Write("볼");
+                    }
+                    else if (input3 == cNumber3)
+                    {
+                        Console.Write("스트라이크");
+                    }
+                    Console.WriteLine();
+                    if (input1 != cNumber1 && input2 != cNumber2 && input2 != cNumber3)
+                    {
+                        Console.WriteLine("아웃!");
+                    }
+                    
+
+                }
+            }
+        }
+
+    }
+}
+
