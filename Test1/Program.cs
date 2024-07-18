e6ddcfca8765100d07cacdac68f01ca506823b82
GuGuJelly <jellypuding@naver.com>
2024-07-18 14:22:19 +0900

    player structure
    플레이어 구조체 작성

---
+++ Test1/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Test1
+{
+    internal class Program
+    {
+        struct Player 
+        {
+            public string name;
+            public int id;
+            public int maxHP;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
