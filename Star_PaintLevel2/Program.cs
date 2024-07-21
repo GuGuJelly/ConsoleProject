40eb587834d4992c6842478ce6a71703957c657d
GuGuJelly <jellypuding@naver.com>
2024-07-21 23:41:47 +0900

    별찍기 레벨2
    * * * * *
    * * * *
    * * *
    * *
    *

---
+++ Star_PaintLevel2/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Star_PaintLevel2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1; i < 6; i++) 
+            {
+                for(int j = 5; j >= i; j--)
+                {
+                    Console.Write("* ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
